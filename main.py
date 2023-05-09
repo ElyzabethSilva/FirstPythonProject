b7d76efbe7821535b4ad85dad92e75c812b87357
Elyzabeth Silva <elyzabeth.alves@hotmail.com>
2023-05-09 17:49:22 -0300

    Downloading and Convertion from Youtube to mp3

---
+++ main.py
@@ -0,0 +1,25 @@
+from pytube import YouTube
+import moviepy.editor as mp
+import re
+import os
+
+link = input("Digite a URL do vídeo que deseja baixar: ")
+path = input("Digite o diretório onde deseja salvar o arquivo: ")
+youtube = YouTube(link)
+
+# Faz o download do arquivo #
+print("Baixando...")
+ys = youtube.streams.filter(only_audio=True).first().download(path)
+print("Download finalizado!")
+
+# Converte de MP3 para MP4 #
+print("Convertendo arquivo...")
+for file in os.listdir(path):
+    if re.search('mp4', file):
+        mp4_path = os.path.join(path, file)
+        mp3_path = os.path.join(path, os.path.splitext(file)[0]+'.mp3')
+        new_file = mp.AudioFileClip(mp4_path)
+        new_file.write_audiofile(mp3_path)
+        os.remove(mp4_path)
+
+print("Arquivo gravado com sucesso!")
